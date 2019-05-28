3e6226b65eb255335460d21601a2f966613283ae
Kayasit Rugtepa <kuyyasit.r@student.chula.ac.th>
2019-05-28 20:28:38 +0700

    add header, fix unity assets

--- projects/manage-storage/src/app/app.module.ts
+++ projects/manage-storage/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
