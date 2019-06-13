34340bd89e1d5c011770a35f7c084006664ffe07
nutthapooms <nutthapoom.s@hotmail.com>
2019-06-13 14:16:27 +0700

    check close

--- projects/locker-kiosk/src/app/locker-option/locker-option.component.ts
+++ projects/locker-kiosk/src/app/locker-option/locker-option.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { detachEmbeddedView } from '@angular/core/src/view';
+import { ContainerDetailComponent } from 'projects/manage-storage/src/app/container/container-detail/container-detail.component';
+import { delay } from 'q';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 
 @Component({
@@ -12,41 +17,64 @@ export class LockerOptionComponent implements OnInit {
   lockers = [1,2,3,4,5,6,7,8,9,10,11]
   displaynumber = [""];
   lockernum = "";
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private route: ActivatedRoute,
+    private router: Router,) { }
   
   ngOnInit() {
-  
+  }
+  @HostListener('document:keydown',['$event'])onkeydownHandler(event: KeyboardEvent){
+    if (event.key === "Enter"){
+      // alert(card_number)
+      card_number = "";
+      // this.router.navigate(['/browse-option'])
+      this.checkLocker(); 
+    }
+    else{
+      if("1234567890_".includes(event.key)){
+        card_number = card_number + event.key
+      }
+    }
   }
   addnum(num = ""){
     this.lockernum = this.lockernum + num
     document.getElementById("displayNum").innerHTML = "Box number :"+this.lockernum;
   }
-openLocker(){
-    // alert(Url+this.lockernum);
-    // Http.open('GET',Url+this.lockernum);
-    // Http.send() 
+  delnum(){
+    this.lockernum = this.lockernum.slice(0, -1);
+    document.getElementById("displayNum").innerHTML = "Box number :"+this.lockernum;    
+  }
+  openLocker(){
     this.http.get(Url+this.lockernum).subscribe(
       data => {
         console.log(data);
       }
     )
     console.log(Url+this.lockernum);
-    document.getElementById("displayNum").innerHTML = "Box number "+this.lockernum+" open!";
-
-    this.lockernum = "";
-
+    document.getElementById("displayNum").innerHTML = "Box number "+this.lockernum+" open!  Scan serial";
   }
-// alert(test());
-   test(){
-    alert(Url+"2");
-    // Http.open('GET',Url+"2");
-    // Http.send() 
+  checkLocker(){
+  this.http.get(Urlcheck+this.lockernum).subscribe(
+    data => {
+      detail = data;
+      console.log("detail is "+detail.result);
+      if(detail.result == 1){
+        alert("close")
+      }
+      console.log(data);
+    }
+  )
+  
+  this.lockernum = "";
 }
+
+
   
 
 }
-
+var card_number = ""
+var detail;
 const Url = '/lockers/open/';
+const Urlcheck = '/lockers/checkclose/';
 const Http = new XMLHttpRequest();
 
 
