99f1a07b3d8083bdda72bd61742e74ca69da6646
nutthapooms <nutthapoom.s@hotmail.com>
2019-06-13 14:56:17 +0700

    check close v2

--- projects/locker-kiosk/src/app/locker-option/locker-option.component.ts
+++ projects/locker-kiosk/src/app/locker-option/locker-option.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { detachEmbeddedView } from '@angular/core/src/view';
 import { ContainerDetailComponent } from 'projects/manage-storage/src/app/container/container-detail/container-detail.component';
 import { delay } from 'q';
 import { ActivatedRoute, Router } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-locker-option',
@@ -14,68 +12,63 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 export class LockerOptionComponent implements OnInit {
-  lockers = [1,2,3,4,5,6,7,8,9,10,11]
+  lockers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
   displaynumber = [""];
   lockernum = "";
-  constructor(private http: HttpClient,private route: ActivatedRoute,
-    private router: Router,) { }
-  
+  constructor(private http: HttpClient, private route: ActivatedRoute,
+    private router: Router, ) { }
+
   ngOnInit() {
   }
-  @HostListener('document:keydown',['$event'])onkeydownHandler(event: KeyboardEvent){
-    if (event.key === "Enter"){
+  @HostListener('document:keydown', ['$event']) onkeydownHandler(event: KeyboardEvent) {
+    if (event.key === "Enter") {
       // alert(card_number)
       card_number = "";
       // this.router.navigate(['/browse-option'])
-      this.checkLocker(); 
+      this.checkLocker();
     }
-    else{
-      if("1234567890_".includes(event.key)){
+    else {
+      if ("1234567890_".includes(event.key)) {
         card_number = card_number + event.key
       }
     }
   }
-  addnum(num = ""){
+  addnum(num = "") {
     this.lockernum = this.lockernum + num
-    document.getElementById("displayNum").innerHTML = "Box number :"+this.lockernum;
+    document.getElementById("displayNum").innerHTML = "Box number :" + this.lockernum;
   }
-  delnum(){
+  delnum() {
     this.lockernum = this.lockernum.slice(0, -1);
-    document.getElementById("displayNum").innerHTML = "Box number :"+this.lockernum;    
+    document.getElementById("displayNum").innerHTML = "Box number :" + this.lockernum;
   }
-  openLocker(){
-    this.http.get(Url+this.lockernum).subscribe(
+  openLocker() {
+    this.http.get(Url + this.lockernum).subscribe(
       data => {
         console.log(data);
       }
     )
-    console.log(Url+this.lockernum);
-    document.getElementById("displayNum").innerHTML = "Box number "+this.lockernum+" open!  Scan serial";
+    console.log(Url + this.lockernum);
+    document.getElementById("numPad").innerHTML = "Box number " + this.lockernum + " open!  Scan serial";
   }
-  checkLocker(){
-  this.http.get(Urlcheck+this.lockernum).subscribe(
-    data => {
-      detail = data;
-      console.log("detail is "+detail.result);
-      if(detail.result == 1){
-        alert("close")
+  checkLocker() {
+    this.http.get(Urlcheck + this.lockernum).subscribe(
+      data => {
+        detail = data;
+        console.log("detail is " + detail.result);
+        if (detail.result == 1) {
+          alert("close")
+          this.router.navigate(['/browse-option'])
+        }
+        console.log(data);
       }
-      console.log(data);
-    }
-  )
-  
-  this.lockernum = "";
-}
-
-
-  
-
+    )
+    this.lockernum = "";
+  }
 }
 var card_number = ""
 var detail;
 const Url = '/lockers/open/';
 const Urlcheck = '/lockers/checkclose/';
-const Http = new XMLHttpRequest();
 
 
 
