72c49b1680ad9d363977f46eba08179afb22aa06
nutthapooms <nutthapoom.s@hotmail.com>
2019-06-13 10:33:44 +0700

    open success ver.2

--- projects/locker-kiosk/src/app/locker-option/locker-option.component.ts
+++ projects/locker-kiosk/src/app/locker-option/locker-option.component.ts
@@ -11,12 +11,35 @@ export class LockerOptionComponent implements OnInit {
 
   constructor(private httpClient:HttpClient) { }
   lockers = [1,2,3,4,5,6,7,8,9,10,11]
-  
+  displaynumber = "";
+  lockernum = "";
   ngOnInit() {
+  
   }
+  addnum(num = ""){
+    this.lockernum = this.lockernum + num
+  }
+openLocker(){
+    // alert(Url+this.lockernum);
+    Http.open('GET',Url+this.lockernum);
+    Http.send() 
+    console.log(Url+this.lockernum);
+    this.lockernum = "";
+
+  }
+// alert(test());
+   test(){
+    alert(Url+"2");
+    Http.open('GET',Url+"2");
+    Http.send() 
+}
   
 
 }
+
+const Url = '/lockers/open/';
+const Http = new XMLHttpRequest();
+
 
 
 
