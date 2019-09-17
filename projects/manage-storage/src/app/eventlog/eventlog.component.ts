cf21ded258996d59cca59cd58e5c0ddbf262b139
nutthapoom <nutthapoom.s@hotmail.com>
2019-09-17 18:10:08 +0700

    change srt data to local

--- projects/manage-storage/src/app/eventlog/eventlog.component.ts
+++ projects/manage-storage/src/app/eventlog/eventlog.component.ts
@@ -4,8 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { LoanDTO, SiteDTO, CountryDTO, ContainerDTO, LoanDTO2, BadgeInfo } from 'src/app/shared/model';
 import { GetDtoService } from 'src/app/shared/get-dto.service';
 import { Location } from '@angular/common';
-
-
+import badgeData from '../badgeInfo.json'
 
 @Component({
     selector: 'app-eventlog',
@@ -21,6 +20,7 @@ export class EventLogComponent implements OnInit {
     country: CountryDTO;
     site: SiteDTO;
     container: ContainerDTO;
+    containerName: string;
     constructor(
         private http: HttpClient,
         private route: ActivatedRoute,
@@ -29,32 +29,55 @@ export class EventLogComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-
         this.siteId = this.route.snapshot.paramMap.get('siteId');
         this.countryId = this.route.snapshot.paramMap.get('countryId');
         this.containerId = this.route.snapshot.paramMap.get('containerId');
+        this.dto.getDTO('countries', this.countryId).subscribe(data => this.country = data)
+        this.dto.getDTO('sites', this.siteId).subscribe(data => this.site = data)
+        this.dto.getDTO('containers', this.containerId).subscribe(data => this.container = data)
         this.http.get<Array<LoanDTO2>>('https://smartlocker.azurewebsites.net/api/admin/loanlist/' + this.containerId).subscribe(data => {
             console.log(data);
-
-            data.forEach(loan => {
-                this.http.get<BadgeInfo>('https://hoesql566.na.xom.com/BadgeEventAPI/api/Badge/ID/'+loan.employee.badgeId, { withCredentials: true }).subscribe(user => {
-                    loan.Company = user.FuncOrgName;
-                    loan.employee.name = user.DisplayName;
+            if (this.container.name != "SRT-Test") {
+                data.forEach(loan => {
+                    this.http.get<BadgeInfo>('https://hoesql566.na.xom.com/BadgeEventAPI/api/Badge/ID/' + loan.employee.badgeId, { withCredentials: true }).subscribe(user => {
+                        loan.Company = user.FuncOrgName;
+                        loan.employee.name = user.DisplayName;
+                    })
+                });
+            }
+            else {
+                data.forEach(loan => {
+                    let Usertemp = this.findUser(loan.employee.badgeId);
+                    if (Usertemp != null) {
+                        loan.Company = Usertemp[0][3].toString() + " : " + Usertemp[0][4].toString();
+                        loan.employee.name = Usertemp[0][1].toString() + " " + Usertemp[0][2].toString();
+                    }
                 })
-            });  
+            }
             this.loanlog = data;
             this.fullloanlog = data;
             console.log(this.loanlog);
 
         })
-        this.dto.getDTO('countries', this.countryId).subscribe(data => this.country = data)
-        this.dto.getDTO('sites', this.siteId).subscribe(data => this.site = data)
-        this.dto.getDTO('containers', this.containerId).subscribe(data => this.container = data)
+
 
         let date = new Date();
         let logFilter_Placeholder = (<HTMLInputElement>document.getElementById("logFilter"));
         logFilter_Placeholder.value = date.getFullYear() + "-" + Math.floor(((date.getMonth() + 1) / 10)) + ((date.getMonth() + 1) % 10);
 
+    }
+    findUser(input: string) {
+        let inputt = input.toString().split("_");
+        // console.log(inputt[1]);
+        let userr = badgeData.filter(badge => badge[0] == inputt[1]);
+        // console.log(this.userr[0][1]);
+        if (userr[0] != null) {
+            return userr
+        }
+        else {
+
+            return null
+        }
     }
     showUserProperty(badge_Id) {
         window.open('https://hoesql566.na.xom.com/BadgeEventAPI/api/Badge/ID/' + badge_Id);
--- projects/manage-storage/src/app/locker/locker-list/locker-list.component.ts
+++ projects/manage-storage/src/app/locker/locker-list/locker-list.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { LockerDTO, ContainerDTO, CountryDTO, SiteDTO, UnitDTO, BadgeInfo } from 'src/app/shared/model';
 import { GetDtoService } from 'src/app/shared/get-dto.service';
 import { Location } from '@angular/common';
+import badgeData from 'src/app/shared/badgeInfo.json'
 
 @Component({
   selector: 'app-locker-list',
@@ -31,23 +32,47 @@ export class LockerListComponent implements OnInit {
     this.countryId = this.route.snapshot.paramMap.get('countryId');
     this.siteId = this.route.snapshot.paramMap.get('siteId');
     this.containerId = this.route.snapshot.paramMap.get('containerId');
+    this.dto.getDTO('countries', this.countryId).subscribe(data => this.country = data)
+    this.dto.getDTO('sites', this.siteId).subscribe(data => this.site = data)
+    this.dto.getDTO('containers', this.containerId).subscribe(data => this.container = data)
     let params = new HttpParams();
-    if(this.containerId) params = params.append('containerId', this.containerId);
+    if (this.containerId) params = params.append('containerId', this.containerId);
 
     this.http.get<Array<LockerDTO>>(`https://smartlocker.azurewebsites.net/api/admin/lockers`, { params: params }).subscribe(data => {
-        console.log(data)
+      console.log(data)
+      if (this.container.name != "SRT-Test") {
         data.forEach(locker => {
-          this.http.get<BadgeInfo>('https://hoesql566.na.xom.com/BadgeEventAPI/api/Badge/ID/'+locker.badgeId, { withCredentials: true }).subscribe(user => {
-              locker.employee = user.DisplayName;
+          this.http.get<BadgeInfo>('https://hoesql566.na.xom.com/BadgeEventAPI/api/Badge/ID/' + locker.badgeId, { withCredentials: true }).subscribe(user => {
+            locker.employee = user.DisplayName;
           })
-      });
-        this.lockers = data;
+        });
+      }
+      else {
+        data.forEach(locker => {
+          if (locker.badgeId!= null) {
+            let Usertemp = this.findUser(locker.badgeId);
+            if (Usertemp != null) {
+              locker.employee = Usertemp[0][1].toString() + " " + Usertemp[0][2].toString();
+            }
+          }
+        })
+      }
+      this.lockers = data;
 
-      })
-      this.dto.getDTO('countries', this.countryId).subscribe(data => this.country = data)
-      this.dto.getDTO('sites', this.siteId).subscribe(data => this.site = data)
-      this.dto.getDTO('containers', this.containerId).subscribe(data => this.container = data)
+    })
+  }
+  findUser(input) {
+    let inputt = input.toString().split("_");
+    // console.log(inputt[1]);
+    let userr = badgeData.filter(badge => badge[0] == inputt[1]);
+    // console.log(this.userr[0][1]);
+    if (userr[0] != null) {
+      return userr
+    }
+    else {
 
+      return null
+    }
   }
   currentLocation() {
     return this.location.path()
