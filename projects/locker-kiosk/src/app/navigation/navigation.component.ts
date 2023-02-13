1987fa4a241519150a26896ae955e12530a70318
nutthapoom suwanwitwaj <nutthapoom.s@hotmail.com>
2023-02-13 23:00:43 +0700

    change client secret

--- projects/locker-kiosk/src/app/navigation/navigation.component.ts
+++ projects/locker-kiosk/src/app/navigation/navigation.component.ts
@@ -52,7 +52,7 @@ export class NavigationComponent implements OnInit {
 
       console.log('getting token')
       this.http.post<any>("https://login.microsoftonline.com/d1ee1acd-bc7a-4bc4-a787-938c49a83906/oauth2/v2.0/token",
-        'grant_type=client_credentials&client_id=a61a1ea8-c8a9-47bf-a84f-caba25dd5879&scope=api://3a4bd202-bede-4dd7-8321-cda964ab6fee/.default&client_secret=cmC8Q~q1Xy~dDPJsRMrlB04f0lnJVfCL~xrjkc1J',
+        'grant_type=client_credentials&client_id=a61a1ea8-c8a9-47bf-a84f-caba25dd5879&scope=api://3a4bd202-bede-4dd7-8321-cda964ab6fee/.default&client_secret=ahL8Q~AZv0FC2ZmYKAea~ov.ScVjrOl~PgWdicmc',
         {
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
